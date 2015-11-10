b70942f957fe9e159d9ea37f82170daa12c35f29
Shawn Looker <shawn.looker@gmail.com>
2015-11-09 21:32:29 -0800

    Added PUT and POST request.body to the log object.

--- index.js
+++ index.js
@@ -7,6 +7,22 @@
 var onHeaders = require('on-headers');
 var dgram = require('dgram');
 
+function getPrettyDate() {
+  var d = new Date();
+  month = '' + (d.getMonth() + 1),
+  day = '' + d.getDate(),
+  year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+
+  return [year, month, day].join('-') + " "+[h, m, s].join(':');
+}
+
 function simpleJsonLogger(host, port, options) {
   return function(req, res, next) {
     var startAt = process.hrtime();
@@ -30,6 +46,11 @@ function simpleJsonLogger(host, port, options) {
         time: getPrettyDate(),
         statusCode: res.statusCode,
       };
+      if (req.method === 'PUT' || req.method === 'POST') {
+        // get put body
+        logObject['requestBody'] = req.body;
+      }
+
       if (options.loggedFromEnv) {
         options.loggedFromEnv.forEach(function(envVar) {
           if (process.env[envVar])
@@ -44,37 +65,18 @@ function simpleJsonLogger(host, port, options) {
         console.log(logEntry);
       }
 
+
       if (host && port) {
-        sendMessage(host, port, logEntry);
+        var message = new Buffer(logEntry);
+        var client = dgram.createSocket('udp4');
+        client.send(message, 0, message.length, port, host, function(err, bytes) {
+          if (err) throw err;
+          client.close();
+        });
+
       }
     });
-    next();
+  next();
   }
 }
 module.exports = simpleJsonLogger;
-
-function sendMessage(host, port, logEntry) {
-  var message = new Buffer(logEntry);
-  var client = dgram.createSocket('udp4');
-  client.send(message, 0, message.length, port, host, function(err, bytes) {
-    if (err) throw err;
-    client.close();
-  });
-}
-module.exports.sendMessage = sendMessage;
-
-function getPrettyDate() {
-  var d = new Date();
-  month = '' + (d.getMonth() + 1),
-  day = '' + d.getDate(),
-  year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  var h = d.getHours();
-  var m = d.getMinutes();
-  var s = d.getSeconds();
-
-  return [year, month, day].join('-') + " "+[h, m, s].join(':');
-}
